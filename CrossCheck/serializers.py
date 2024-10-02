a7b5e6ca51c25ba157e784b8d760d077e2e515dc
fvinodh1978 <f.vinodhfranklin@gmail.com>
2024-10-01 21:36:33 -0400

    Added User registration API

--- CrossCheck/serializers.py
+++ CrossCheck/serializers.py
@@ -1,15 +1,28 @@
 from django.contrib.auth import authenticate
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 from CrossCheck.models import TestCases, Users
 
+class AuthUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
 
 class TestCasesSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestCases
         fields = ['id', 'testCaseName', 'description', 'type']
 
-class UsersSerializer(serializers.ModelSerializer):
+class ActiveUsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ['username', 'email', 'password', 'createdOn']
@@ -22,16 +35,20 @@ class UserLoginSerializer(serializers.Serializer):
     def validate(self, data):
         username = data.get('username')
         password = data.get('password')
-
+        print("Vinodh1")
         if username and password:
             # Authenticate the user
+            print("Vinodh2")
             user = authenticate(username=username, password=password)
             if user:
+                print("Vinodh3")
                 if not user.is_active:
+                    print("Vinodh4")
                     raise serializers.ValidationError("User account is disabled.")
                 # return user
                 return data
             else:
+                print("Vinodh5")
                 raise serializers.ValidationError("Unable to log in with provided credentials.")
         else:
             raise serializers.ValidationError("Must include 'username' and 'password'.")
